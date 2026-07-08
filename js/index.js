1590d6ea8938e0597069d3737d1ffb2ad3234f25
mdalaminemoon <mdalaminemon413@gmail.com>
2026-07-09 01:56:38 +0600

    JS Calculator Done

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let num1 = prompt("Enter 1st Number:");
+let choice = prompt("What do you want to do?(Enter +, -, *, /,%,>,<):");
+let num2 = prompt("Enter 2nd Number:");
+let result;
+if (choice === "+")
+{
+    result = Number(num1) + Number(num2);
+    console.log("sum: " , result);
+}
+else if (choice === "-")
+{
+    result = Number(num1) - Number(num2);
+    console.log("sub: " , result);
+} 
+else if (choice === "*") 
+{
+    result = Number(num1) * Number(num2);
+    console.log("mul: " , result);
+} 
+else if (choice === "/") 
+{
+    result = Number(num1) / Number(num2);
+    console.log("div: " , result);
+} 
+else if (choice === "%") 
+{
+    result = Number(num1) % Number(num2);
+    console.log("div: " , result);
+}
+else if (choice === ">") 
+{
+    result = Number(num1) >> Number(num2);
+    console.log("div: " , result);
+}
+else if (choice === "<") 
+{
+    result = Number(num1) << Number(num2);
+    console.log("div: " , result);
+}
+else {"Invalid Choice"}
